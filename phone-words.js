d566e367c39a7473b2c7826b7798b8cab06a26fb
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-12 03:57:00 +0000

    completed recursion algorithm

--- phone-words.js
+++ phone-words.js
@@ -1,18 +1,56 @@
+var inputArgs = process.argv.slice(2);
+
 function makePhoneWords(inputNumber) {
+
   inputNumber = String(inputNumber);
 
   if (inputNumber.length !== 7 || inputNumber.search('0') !== -1 || inputNumber.search('1') !== -1) {
-    return "Please enter a 7-digit phone number that does not contain 0s or 1s."
+    console.log('Please enter a 7-digit phone number that does not contain 0s or 1s.');
+    return;
   }
 
+  var digitLetters = {
+    2: ['A', 'B', 'C'],
+    3: ['D', 'E', 'F'],
+    4: ['G', 'H', 'I'],
+    5: ['J', 'K', 'L'],
+    6: ['M', 'N', 'O'],
+    7: ['P', 'R', 'S'],
+    8: ['T', 'U', 'V'],
+    9: ['W', 'X', 'Y']
+  };
+
+  var count = 0;
+
+  function recursiveAnswer (prefix, currentSlot) {
+    if (currentSlot === inputNumber.length) {
+      console.log(prefix);
+      count++;
+      return '';
+    }
+
+    for (var i = 0; i < 3; i++) {
+      prefix += recursiveAnswer(prefix + digitLetters[inputNumber[currentSlot]][i], currentSlot + 1);
+    }
+    return '';
+  }
+
+  recursiveAnswer('', 0);
+  console.log(count);
 
 }
 
 
+
 // Test Cases
 
-console.log(makePhoneWords(1998794));
-console.log(makePhoneWords(2028794));
-console.log(makePhoneWords(128794));
+// for (var i = 0; i < inputArgs.length; i++) {
+//   console.log(makePhoneWords(inputArgs[i]));
+// }
 
-console.log(makePhoneWords(2555247));
+// improper input
+makePhoneWords(1998794);
+makePhoneWords(2028794);
+makePhoneWords(128794);
+
+makePhoneWords(2559399);
